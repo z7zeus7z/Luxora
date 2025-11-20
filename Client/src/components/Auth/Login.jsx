cede79acb1442e6f0c6063048eb3674b572ed6c5
ZaidSabbah <120690909+z7zeus7z@users.noreply.github.com>
2025-11-20 12:10:50 -0900

    Update Login.jsx fetch url

--- Client/src/components/Auth/Login.jsx
+++ Client/src/components/Auth/Login.jsx
@@ -25,7 +25,7 @@ const Login = (props) => {
         setLoading(true);
         try
         {
-            const res = await fetch("http://localhost:5000/api/users/login",{
+            const res = await fetch("https://luxora-backend-0gll.onrender.com/api/users/login",{
                 method:"POST",
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify({
